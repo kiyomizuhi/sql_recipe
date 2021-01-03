8e2053641c1c6525c4a53978014cb1348eb9a696
hiroyukiinoue <hiroyukiinoue1444@gmail.com>
2021-01-03 16:45:32 +0900

    still in ch04_02

---
+++ script/ch04_02/query_02.sql
@@ -0,0 +1,25 @@
+WITH monthly_sales AS (
+    SELECT
+        category
+      , SUM(price) AS category_amount
+    FROM
+        purchase_detail_log
+    WHERE
+        dt BETWEEN '2015-12-01' AND '2015-12-31'
+    GROUP BY
+        category
+)
+
+SELECT
+    category
+  , category_amount
+  , 100 * category_amount / SUM(category_amount) OVER() AS ratio
+  , 100 * SUM(category_amount)
+        OVER(ORDER BY category_amount DESC)
+        / SUM(category_amount) OVER() AS cum_ratio
+
+FROM
+    monthly_sales
+ORDER BY
+    category_amount DESC
+;
